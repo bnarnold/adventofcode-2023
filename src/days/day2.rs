f42962d51ecc55ba7a7a4ce8f457a8095e9ec4a5
Bertram Arnold <bertram.arnold@tngtech.com>
2023-12-02 16:15:47 +0100

    get rid of allocations in day 2

--- src/days/day2.rs
+++ src/days/day2.rs
@@ -2,7 +2,7 @@ use crate::util::prelude::*;
 use nom::{
     branch::alt,
     character::complete::{newline, space1, u32},
-    combinator::eof,
+    combinator::{eof, success},
     multi::many0,
     sequence::tuple,
     IResult, Parser,
@@ -15,91 +15,146 @@ use nom_supreme::{
     ParserExt,
 };
 
-#[derive(Debug, Clone, Copy, Default, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy)]
+enum Color {
+    Red,
+    Green,
+    Blue,
+}
+
+#[derive(Debug, Clone, Default, PartialEq, Eq)]
 struct CubeSet {
     red: u32,
     green: u32,
     blue: u32,
 }
 
-impl CubeSet {
-    fn max(&self, other: &Self) -> Self {
-        CubeSet {
-            red: self.red.max(other.red),
-            green: self.green.max(other.green),
-            blue: self.blue.max(other.blue),
+impl Extend<(u32, Color)> for CubeSet {
+    fn extend<T: IntoIterator<Item = (u32, Color)>>(&mut self, iter: T) {
+        for (count, color) in iter {
+            match color {
+                Color::Red => self.red = count,
+                Color::Green => self.green = count,
+                Color::Blue => self.blue = count,
+            }
+        }
+    }
+}
+
+impl Extend<CubeSet> for CubeSet {
+    fn extend<T: IntoIterator<Item = CubeSet>>(&mut self, iter: T) {
+        for set in iter {
+            self.red = self.red.max(set.red);
+            self.green = self.green.max(set.green);
+            self.blue = self.blue.max(set.blue);
         }
     }
 }
 
 #[derive(Debug)]
-struct Game {
-    id: u32,
-    sets: Vec<CubeSet>,
+struct IsGameUnderMax<const RED: u32, const BLUE: u32, const GREEN: u32> {
+    is_under_max: bool,
+}
+
+impl<const RED: u32, const BLUE: u32, const GREEN: u32> Extend<CubeSet>
+    for IsGameUnderMax<RED, BLUE, GREEN>
+{
+    fn extend<T: IntoIterator<Item = CubeSet>>(&mut self, iter: T) {
+        for set in iter {
+            self.is_under_max &= set.red <= RED && set.blue <= BLUE && set.green <= GREEN
+        }
+    }
+}
+
+#[derive(Debug, Default)]
+struct GamesUnderMaxCounter<const RED: u32, const BLUE: u32, const GREEN: u32> {
+    sum: u32,
+}
+
+impl<const RED: u32, const BLUE: u32, const GREEN: u32>
+    Extend<(u32, IsGameUnderMax<RED, GREEN, BLUE>)> for GamesUnderMaxCounter<RED, BLUE, GREEN>
+{
+    fn extend<T: IntoIterator<Item = (u32, IsGameUnderMax<RED, GREEN, BLUE>)>>(&mut self, iter: T) {
+        self.sum += iter
+            .into_iter()
+            .filter_map(|(id, is_under)| is_under.is_under_max.then_some(id))
+            .sum::<u32>()
+    }
+}
+
+#[derive(Debug, Default)]
+struct GamePowerCounter {
+    sum: u32,
+}
+
+impl Extend<(u32, CubeSet)> for GamePowerCounter {
+    fn extend<T: IntoIterator<Item = (u32, CubeSet)>>(&mut self, iter: T) {
+        self.sum += iter
+            .into_iter()
+            .map(|(_, CubeSet { red, green, blue })| red * green * blue)
+            .sum::<u32>()
+    }
+}
+
+impl<const RED: u32, const BLUE: u32, const GREEN: u32> Default
+    for IsGameUnderMax<RED, BLUE, GREEN>
+{
+    fn default() -> Self {
+        Self { is_under_max: true }
+    }
 }
 
 fn parse_cube_set(input: &str) -> IResult<&str, CubeSet, ErrorTree<&str>> {
-    let parse_color =
-        tuple((u32, space1, alt((tag("red"), tag("green"), tag("blue"))))).context("color count");
-    parse_separated_terminated(
+    let parse_color = tuple((
+        u32,
+        space1,
+        alt((
+            tag("red").value(Color::Red),
+            tag("green").value(Color::Green),
+            tag("blue").value(Color::Blue),
+        )),
+    ))
+    .map(|(count, _, color)| (count, color))
+    .context("color count");
+    collect_separated_terminated(
         parse_color,
         tag(", "),
-        alt((tag("; "), tag("\n"), eof)),
-        CubeSet::default,
-        |mut set, (count, _, color)| {
-            match color {
-                "red" => set.red = count,
-                "green" => set.green = count,
-                "blue" => set.blue = count,
-                _ => unreachable!("parse succeeded but got unexpected color"),
-            }
-            set
-        },
+        tag("; ").or(alt((tag("\n"), eof)).peek()),
     )
     .context("set of cubes")
     .parse(input)
 }
 
-fn parse_game(input: &str) -> IResult<&str, Game, ErrorTree<&str>> {
+fn parse_game<C>(input: &str) -> IResult<&str, (u32, C), ErrorTree<&str>>
+where
+    C: Default + Extend<CubeSet>,
+{
     tuple((
         u32.preceded_by(tag("Game "))
             .terminated(tag(": "))
             .context("Game ID"),
-        many0(parse_cube_set).context("Cube sets"),
+        collect_separated_terminated(parse_cube_set, success(()), tag("\n").or(eof)),
     ))
-    .map(|(id, sets)| Game { id, sets })
     .context("Game")
     .parse(input)
 }
 
-fn parse_input(input: &str) -> Result<Vec<Game>, ErrorTree<Location>> {
-    final_parser(many0(parse_game))(input)
+fn parse_input<C1, C2>(input: &str) -> Result<C1, ErrorTree<Location>>
+where
+    C1: Default + Extend<(u32, C2)>,
+    C2: Default + Extend<CubeSet>,
+{
+    final_parser(collect_separated_terminated(parse_game, success(()), eof))(input)
 }
 
 pub fn level1(input: &str) -> u32 {
-    let max_set = CubeSet {
-        red: 12,
-        green: 13,
-        blue: 14,
-    };
-    let games = parse_input(input).expect("parse error");
-    games
-        .into_iter()
-        .filter(|game| game.sets.iter().all(|set| set.max(&max_set) == max_set))
-        .map(|game| game.id)
-        .sum()
+    let result: GamesUnderMaxCounter<12, 13, 14> = parse_input(input).expect("parse error");
+    result.sum
 }
 
 pub fn level2(input: &str) -> u32 {
-    parse_input(input)
-        .expect("Parse error")
-        .into_iter()
-        .map(|Game { sets, .. }| {
-            sets.iter()
-                .fold(CubeSet::default(), |acc, set| acc.max(set))
-        })
-        .map(|CubeSet { red, green, blue }| red * green * blue)
-        .sum()
+    let result: GamePowerCounter = parse_input(input).expect("parse error");
+    result.sum
 }
 
 #[cfg(test)]
